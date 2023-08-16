893590c8b682f1d49ab74bd415ebe57aa6b878fe
Laryssa Ramos <laryssaramos284@gmail.com>
2023-08-16 15:31:07 -0300

    primeiro commit do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+//FUNCTION TOGGLE LOGO
+
+function scrollLogo(){
+
+    const logo = document.getElementById('logo');
+    const header = document.getElementById('header');
+    const btn = document.getElementById('menu-btn');
+
+    if(this.scrollY >= 70){
+        logo.classList.add('scroll-logo');
+        header.classList.add('scroll-header');
+        btn.classList.add('scroll-btn');
+    }
+    else{
+        logo.classList.remove('scroll-logo');
+        header.classList.remove('scroll-header');
+        btn.classList.remove('scroll-btn');
+
+    }
+}
+
+
+window.addEventListener('scroll', scrollLogo)
+
+
+//FUNCTION CAROUSEL
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carousel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+setInterval(carousel, 2000);
+
+
+//FUNCTION MENU MOBILE
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+ 
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+}
